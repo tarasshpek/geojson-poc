0ca97a3262f0b9c6acf33b73a9a2f9d2c74cc4aa
Taras Shpek <taras.shpek@gmail.com>
2016-11-30 02:44:03 +0200

    TS: Add logging

--- src/main/java/com/mycompany/controllers/WeatherStationController.java
+++ src/main/java/com/mycompany/controllers/WeatherStationController.java
@@ -3,6 +3,9 @@ package com.mycompany.controllers;
 import com.mycompany.models.Coordinates;
 import com.mycompany.models.WeatherStation;
 import com.mycompany.services.WeatherStationService;
+import lombok.extern.java.Log;
+import lombok.extern.log4j.Log4j;
+import lombok.extern.slf4j.Slf4j;
 import org.geojson.Feature;
 import org.geojson.LngLatAlt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,6 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class WeatherStationController {
 
     @Autowired
@@ -22,6 +26,7 @@ public class WeatherStationController {
 
     @PutMapping("/weatherstations")
     public List<Feature> getStations(@RequestBody List<Coordinates> coordinates){
+        log.info("PUT /weatherstations " + coordinates);
         List<WeatherStation> stations = service.getStationsWithProximity(coordinates);
         return service.convertStationsToFeatures(stations);
     }
